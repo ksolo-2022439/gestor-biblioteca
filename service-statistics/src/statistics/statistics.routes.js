c8821740ff54f02c4f6ab0b8d018b4ff0b18d9d5
ksolo-2022439 <ksolorzano-2022439@kinal.edu.gt>
2026-07-14 10:43:03 -0600

    feat: Finaliza integracion de servicio de estadisticas y documentacion de Scriba

--- service-statistics/src/statistics/statistics.routes.js
+++ service-statistics/src/statistics/statistics.routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { getStatistics, getCategories } from './statistics.controller.js';
+import {
+  getStatistics,
+  getCategories,
+  getRecommendations,
+  getSummary
+} from './statistics.controller.js';
 import { validateJWT } from '../../middlewares/validate-jwt.js';
 
 const router = Router();
 
 router.get('/', validateJWT, getStatistics);
 router.get('/categories', validateJWT, getCategories);
+router.get('/recommendations/:category', validateJWT, getRecommendations);
+router.get('/summary', validateJWT, getSummary);
 
 export default router;
